355ee940615d6db31fac50a2f47ef5d6c3ed1b4a
Todd Bruss <todd@starplayrx.com>
2020-06-10 22:42:41 -0400

    0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Swift4me_InsertXjs
-Swift4.me Fast Fun Free Swift5+ Snippets build on the InsertX.JS framework
+Swift4.me Fast Fun Free Swift5+ Snippets built on the InsertX.JS framework
+
+InsertX.JS Alpha v0.0.2 is included in this repo
+It is a minimalistic dynamic framework for building simple websites
+
+It uses fetch to build dynamic web pages from static html content.
+
+To use fetch you must run the page from an http web server.
+
+There many out there. I use Perfect3 from PerfectlySoft, but you use any HTTP web server like Apache, NGINX, Node, Perfect, Vapor, Kitura, etc.
+
+This is how insertX places HTML content:
+
+<!-- insert html file -->
+<insert-X
+	data-fold="snippets" 		<!-- folder with relative path ./ -->
+	data-file="assemblyline"     <!-- name of the file -->
+	data-type="html">	       <!-- type of file (txt and html is supported) -->
+</insert-X>
+
+InsertX can also draw minimalistic buttons via this simple markup. Button-X's buttons are fully compatible with desktop and touch devices. And both will have the same look at feel. They even have the same behaviors on a touch device as they do on the desktop!
+
+<button-X
+	data-mini="default" 		 <!-- style of the button (currently only option is default) -->
+	data-clik="text2clip"             <!-- the button action -->
+	data-args="assemblyline"    <!-- arguments, this currently a String,
+                                                            JS variable support is under way -->
+	data-name="Clipboard">     <!-- name or title of the button -->
+</button-X>
+
+InsertX.js is open source and is currently only available through this repo.
+
+It is designed to be a standable framework with it's own CSS, HTML and JS. The JS is prorgammed in ES6.
+
+Initially InsertX was going to use Bootstrap but I was not impressed that BS4.5 still has dependencies to jQuery. I also feel that BS is grown way beyond a simple CSS and HMTL framework. So, InsertX decided to go solo offering it own flavor of UI minimalism.
+
+InsertX is designed to make simple websites fast and fun! I want this framework to be easy and and simple to add your own content.
+
+As Swift4.me grows, which is powered by InsertX, so will InsertX.js. Thank you!
+
+Sincerely,
+
+Todd Bruss
+starplayrx.com
+swift4.me
---
+++ www_swift_4_me/js/insertX.js
@@ -0,0 +1,125 @@
+/*
+ insertX.js v0.0.2a (c) 2020 by _Todd _Bruss Swift4.me Swift4me.com
+ the UltraLite JS, HTML and CSS framework for building dynamic websites
+ Presenting Blue Minimal Dark UI by Todd Bruss (c) 2020
+ */
+
+//MARK: _O => _Objects by Reference
+const _O = {
+    doc  : document,
+    win  : window
+}
+
+//MARK: _A => _Arrays
+const _A = {
+    empty           : [],
+}
+
+//MARK: _S => _Strings
+const _S = {
+    X               : 'insert-X',
+    B               : 'button-X',
+    end             : 'afterend',
+    copy            : 'copy',
+    empty           : '',
+    dot             : ".",
+    cd              : "./",
+    slash           : "/",
+    menu            : 'selectmenu',
+}
+
+//MARK: _I => _Integers
+const _I = {
+    zero            : 0,
+    delay           : 200,
+}
+
+//MARK: _B => _Buttons
+const _B = {
+    mini               : 'minimal',
+    default            : 'default',
+    dash               : '-',
+    up                 : 'up',
+    down               : 'down',
+    this               : 'this',
+    comma              : ',',
+}
+
+//MARK: To Do Refactor this, so it's neater and introduce customer variables instead of strings
+const insertBtns = _ => {
+    _A.empty.forEach.call(_O.doc.getElementsByTagName(_), x => {
+        
+        let mini = x.dataset.mini
+        let clik = x.dataset.clik
+        let args = x.dataset.args
+        let name = x.dataset.name
+             
+        let click = clik + "('" + args + "') "
+        let pattn = _B.this + _B.comma + "'" + _B.mini + _B.dash + mini
+        
+        //MARK: To Do - loop over the guts for this button
+        let bttn = "<button onclick=" + click + "onmouseup=minimalButton(" +
+        pattn + "') onmousedown=minimalButton(" +
+        pattn + _B.dash + _B.down + "') onmouseleave=minimalButton(" +
+        pattn + "') ontouchstart=minimalButton(" +
+        pattn + _B.dash + _B.down + "') ontouchend=minimalButton(" +
+        pattn + "') class='" + _B.mini + _B.dash + mini + "'> " + name + " </button>"
+
+        x.insertAdjacentHTML(_S.end, bttn)
+        
+    } )
+}
+
+const insertFile = _ => {
+    _A.empty.forEach.call(_O.doc.getElementsByTagName(_), x =>
+        fetch(_S.cd    + x.dataset.fold +
+              _S.slash + x.dataset.file +
+              _S.dot   + x.dataset.type )
+        .then(_ => _.text())
+        .then(_ => x.insertAdjacentHTML(_S.end, _))
+    )
+}
+
+//MARK: Reset Main Menu
+const reset = menu => {
+    let item = _O.doc.getElementById(menu)
+    item.selectedIndex = 0
+}
+
+//MARK: Startup menu location
+const startup = _ => {
+    let item = _O.doc.getElementById(_)
+    let menu = item[0]
+    _O.win.location = `#${menu.value}`
+}
+
+//MARK: copy text to clipboard
+const text2clip = utf8 => {
+    let text = _O.doc.getElementById(utf8)
+    text.select()
+    _O.doc.execCommand(_S.copy)
+    text.blur()
+}
+
+//MARK: Select menu with snap option
+const select = (menu,snap) => {
+    _O.win.location = `#${menu.value}`
+    
+    //snap to empty string or selected value
+    menu.value = snap ? _S.empty : menu.value
+}
+
+//MARK: Select menu with snap option
+const minimalButton = (obj, def) => {
+    obj.className = def
+    setTimeout( _ => obj.blur(), _I.delay )
+}
+
+//MARK: Startup insertX.js
+const init = _ =>
+insertFile(_S.X)
+insertBtns(_S.B)
+reset(_S.menu)
+startup(_S.menu)
+
+init()
